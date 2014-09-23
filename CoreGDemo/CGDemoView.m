93a1a0ac64d96b993c35a6ffc9b015d184f3dd1d
linfengchina <linfeng@linfeng-iOS.local>
2014-09-23 17:36:28 +0800

    UPDATE
    UPDATE

--- CoreGDemo/CGDemoView.m
+++ CoreGDemo/CGDemoView.m
@@ -8,6 +8,8 @@
 
 #import "CGDemoView.h"
 
+#define TILE_SIZE 20
+
 @implementation CGDemoView
 
 - (id)initWithFrame:(CGRect)frame
@@ -20,12 +22,226 @@
     return self;
 }
 
+//void drawColoredTile(void *info,CGContextRef context){
+//    // 有颜色填充，在这里设置填充色
+//    CGContextSetRGBFillColor(context, 254.0/255.0, 52.0/255.0, 90.0/255.0, 1);
+//    CGContextFillRect(context, CGRectMake(0, 0, TILE_SIZE, TILE_SIZE));
+//    CGContextFillRect(context, CGRectMake(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE));
+//    
+//    CGContextSetRGBFillColor(context, 0.0/255.0, 160.0/255.0, 190.0/255.0, 1);
+//    CGContextFillRect(context, CGRectMake(TILE_SIZE, 0, TILE_SIZE, TILE_SIZE));
+//    CGContextFillRect(context, CGRectMake(0, TILE_SIZE, TILE_SIZE, TILE_SIZE));
+//}
+//
+//void drawTile(void *info,CGContextRef context){
+//    CGContextFillRect(context, CGRectMake(0, 0, TILE_SIZE, TILE_SIZE));
+//    CGContextFillRect(context, CGRectMake(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE));
+//}
+
 - (void)drawRect:(CGRect)rect
 {
     // 获取绘图上下文
     CGContextRef context = UIGraphicsGetCurrentContext();
     
+    // 保存初始状态
+    CGContextSaveGState(context);
+    // 形变第一步：图形上下文向右平移80
+    CGContextTranslateCTM(context, 80, 0);
+    // 形变第二步：缩放0.8
+    CGContextScaleCTM(context, 0.8, 0.8);
+    // 形变第三步：旋转
+    CGContextRotateCTM(context, M_PI_4/4);
+    
     UIImage *image=[UIImage imageNamed:@"1.png"];
+    [image drawInRect:CGRectMake(0, 50, 240, 300)];
+    
+    //恢复到初始状态
+    CGContextRestoreGState(context);
+    
+    
+//    // 创建RGB颜色空间
+//    CGColorSpaceRef rgbSpace= CGColorSpaceCreateDeviceRGB();
+//    // 使用RGB颜色空间创建填充颜色空间
+//    CGColorSpaceRef colorSpace=CGColorSpaceCreatePattern(rgbSpace);
+//    // 将填充色颜色空间设置为模式填充的颜色空间
+//    CGContextSetFillColorSpace(context, colorSpace);
+//    
+//    //填充模式回调函数结构体
+//    CGPatternCallbacks callback={0, &drawTile, NULL};
+//    
+//    // 创建单位图形 参数依次为:
+//    // info:传递给callback的参数
+//    // bounds:单位图形绘制大小
+//    // matrix:形变
+//    // xStep:单位图形分配宽度
+//    // yStep:单位图形分配高度
+//    // tiling:三种模式，控制是否扭曲
+//    // isClored:绘制的瓷砖是否指定颜色(对于无颜色瓷砖此处指定位false)
+//    // callbacks:设置回调函数
+//    CGPatternRef pattern=CGPatternCreate(NULL, CGRectMake(0, 0, 2*TILE_SIZE, 2*TILE_SIZE), CGAffineTransformIdentity,2*TILE_SIZE,2*TILE_SIZE, kCGPatternTilingNoDistortion, false, &callback);
+//    
+//    CGFloat components[]={254.0/255.0,52.0/255.0,90.0/255.0,1.0};
+//    // 注意最后一个参数对于无颜色填充模式指定为当前颜色空间颜色数据
+//    CGContextSetFillPattern(context, pattern, components);
+//    UIRectFill(CGRectMake(0, 0, 320, 568));
+//    
+//    CGColorSpaceRelease(rgbSpace);
+//    CGColorSpaceRelease(colorSpace);
+//    CGPatternRelease(pattern);
+    
+//    // 创建填充的颜色空间 有颜色填充 传NULL
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreatePattern(NULL);
+//    // 设置颜色空间
+//    CGContextSetFillColorSpace(context, colorSpace);
+//    
+//    // 设置实际创建单位图形的回调函数
+//    CGPatternCallbacks callback={0, &drawColoredTile, NULL};
+//    
+//    // 创建单位图形 参数依次为:
+//    // info:传递给callback的参数
+//    // bounds:单位图形绘制大小
+//    // matrix:形变
+//    // xStep:单位图形分配宽度
+//    // yStep:单位图形分配高度
+//    // tiling:三种模式，控制是否扭曲
+//    // isClored:绘制的瓷砖是否指定颜色(对于有颜色瓷砖此处指定位true)
+//    // callbacks:设置回调函数
+//    CGPatternRef pattern=CGPatternCreate(NULL, CGRectMake(0, 0, 2*TILE_SIZE, 2*TILE_SIZE), CGAffineTransformIdentity, 2*TILE_SIZE,2*TILE_SIZE, kCGPatternTilingNoDistortion, true, &callback);
+//    
+//    CGFloat alpha = 1.0;
+//    // 注意最后一个参数对于有颜色瓷砖指定为透明度的参数地址，对于无颜色瓷砖则指定当前颜色空间对应的颜色数组
+//    CGContextSetFillPattern(context, pattern, &alpha);
+//    
+//    UIRectFill(CGRectMake(0, 0, 320, 480));
+//    
+//    // 释放
+//    CGColorSpaceRelease(colorSpace);
+//    CGPatternRelease(pattern);
+    
+//    CGRect myRect= CGRectMake(0, 130.0, 320.0, 50.0);
+//    CGRect rect1= CGRectMake(0, 390.0, 320.0, 50.0);
+//    
+//    
+//    CGRect rect2=CGRectMake(20, 50.0, 10.0, 250.0);
+//    CGRect rect3=CGRectMake(40.0, 50.0, 10.0, 250.0);
+//    CGRect rect4=CGRectMake(60.0, 50.0, 10.0, 250.0);
+//    CGRect rect5=CGRectMake(80.0, 50.0, 10.0, 250.0);
+//    CGRect rect6=CGRectMake(100.0, 50.0, 10.0, 250.0);
+//    CGRect rect7=CGRectMake(120.0, 50.0, 10.0, 250.0);
+//    CGRect rect8=CGRectMake(140.0, 50.0, 10.0, 250.0);
+//    CGRect rect9=CGRectMake(160.0, 50.0, 10.0, 250.0);
+//    CGRect rect10=CGRectMake(180.0, 50.0, 10.0, 250.0);
+//    CGRect rect11=CGRectMake(200.0, 50.0, 10.0, 250.0);
+//    CGRect rect12=CGRectMake(220.0, 50.0, 10.0, 250.0);
+//    CGRect rect13=CGRectMake(240.0, 50.0, 10.0, 250.0);
+//    CGRect rect14=CGRectMake(260.0, 50.0, 10.0, 250.0);
+//    CGRect rect15=CGRectMake(280.0, 50.0, 10.0, 250.0);
+//    
+//    CGRect rect16=CGRectMake(30.0, 310.0, 10.0, 250.0);
+//    CGRect rect17=CGRectMake(50.0, 310.0, 10.0, 250.0);
+//    CGRect rect18=CGRectMake(70.0, 310.0, 10.0, 250.0);
+//    CGRect rect19=CGRectMake(90.0, 310.0, 10.0, 250.0);
+//    CGRect rect20=CGRectMake(110.0, 310.0, 10.0, 250.0);
+//    CGRect rect21=CGRectMake(130.0, 310.0, 10.0, 250.0);
+//    CGRect rect22=CGRectMake(150.0, 310.0, 10.0, 250.0);
+//    CGRect rect23=CGRectMake(170.0, 310.0, 10.0, 250.0);
+//    CGRect rect24=CGRectMake(190.0, 310.0, 10.0, 250.0);
+//    CGRect rect25=CGRectMake(210.0, 310.0, 10.0, 250.0);
+//    CGRect rect26=CGRectMake(230.0, 310.0, 10.0, 250.0);
+//    CGRect rect27=CGRectMake(250.0, 310.0, 10.0, 250.0);
+//    CGRect rect28=CGRectMake(270.0, 310.0, 10.0, 250.0);
+//    CGRect rect29=CGRectMake(290.0, 310.0, 10.0, 250.0);
+//    
+//    
+//    [[UIColor yellowColor]set];
+//    UIRectFill(myRect);
+//    
+//    [[UIColor greenColor]setFill];
+//    UIRectFill(rect1);
+//    
+//    [[UIColor redColor]setFill];
+//    UIRectFillUsingBlendMode(rect2, kCGBlendModeClear);
+//    UIRectFillUsingBlendMode(rect3, kCGBlendModeColor);
+//    UIRectFillUsingBlendMode(rect4, kCGBlendModeColorBurn);
+//    UIRectFillUsingBlendMode(rect5, kCGBlendModeColorDodge);
+//    UIRectFillUsingBlendMode(rect6, kCGBlendModeCopy);
+//    UIRectFillUsingBlendMode(rect7, kCGBlendModeDarken);
+//    UIRectFillUsingBlendMode(rect8, kCGBlendModeDestinationAtop);
+//    UIRectFillUsingBlendMode(rect9, kCGBlendModeDestinationIn);
+//    UIRectFillUsingBlendMode(rect10, kCGBlendModeDestinationOut);
+//    UIRectFillUsingBlendMode(rect11, kCGBlendModeDestinationOver);
+//    UIRectFillUsingBlendMode(rect12, kCGBlendModeDifference);
+//    UIRectFillUsingBlendMode(rect13, kCGBlendModeExclusion);
+//    UIRectFillUsingBlendMode(rect14, kCGBlendModeHardLight);
+//    UIRectFillUsingBlendMode(rect15, kCGBlendModeHue);
+//    UIRectFillUsingBlendMode(rect16, kCGBlendModeLighten);
+//    
+//    UIRectFillUsingBlendMode(rect17, kCGBlendModeLuminosity);
+//    UIRectFillUsingBlendMode(rect18, kCGBlendModeMultiply);
+//    UIRectFillUsingBlendMode(rect19, kCGBlendModeNormal);
+//    UIRectFillUsingBlendMode(rect20, kCGBlendModeOverlay);
+//    UIRectFillUsingBlendMode(rect21, kCGBlendModePlusDarker);
+//    UIRectFillUsingBlendMode(rect22, kCGBlendModePlusLighter);
+//    UIRectFillUsingBlendMode(rect23, kCGBlendModeSaturation);
+//    UIRectFillUsingBlendMode(rect24, kCGBlendModeScreen);
+//    UIRectFillUsingBlendMode(rect25, kCGBlendModeSoftLight);
+//    UIRectFillUsingBlendMode(rect26, kCGBlendModeSourceAtop);
+//    UIRectFillUsingBlendMode(rect27, kCGBlendModeSourceIn);
+//    UIRectFillUsingBlendMode(rect28, kCGBlendModeSourceOut);
+//    UIRectFillUsingBlendMode(rect29, kCGBlendModeXOR);
+//    
+//    CGContextSetBlendMode(context, <#CGBlendMode mode#>)
+    
+//    //裁切处一块矩形用于显示，注意必须先裁切再调用渐变
+//    //CGContextClipToRect(context, CGRectMake(20, 50, 280, 300));
+//    //裁切还可以使用UIKit中对应的方法
+//    UIRectClip(CGRectMake(20, 50, 280, 300));
+    
+//    // 使用rgb颜色空间
+//    CGColorSpaceRef colorSpace=CGColorSpaceCreateDeviceRGB();
+//    
+//    // 同线性渐变
+//    CGFloat compoents[12]={
+//        248.0/255.0,86.0/255.0,86.0/255.0,1,
+//        249.0/255.0,127.0/255.0,127.0/255.0,1,
+//        1.0,1.0,1.0,1.0
+//    };
+//    CGFloat locations[3]={0,0.3,1.0};
+//    CGGradientRef gradient= CGGradientCreateWithColorComponents(colorSpace, compoents, locations, 3);
+//    
+//    // startCenter:起始点位置
+//    // startRadius:起始半径（通常为0，否则在此半径范围内容无任何填充）
+//    // endCenter:终点位置（通常和起始点相同，否则会有偏移）
+//    // endRadius:终点半径（也就是渐变的扩散长度）
+//    // options:绘制方式,kCGGradientDrawsBeforeStartLocation 开始位置之前就进行绘制，但是到结束位置之后不再绘制，kCGGradientDrawsAfterEndLocation开始位置之前不进行绘制，但到结束点之后继续填充
+//    CGContextDrawRadialGradient(context, gradient, CGPointMake(160, 284), 0, CGPointMake(165, 289), 150, kCGGradientDrawsAfterEndLocation);
+//    //释放颜色空间
+//    CGColorSpaceRelease(colorSpace);
+    
+//    //使用rgb颜色空间
+//    CGColorSpaceRef colorSpace=CGColorSpaceCreateDeviceRGB();
+//    
+//    // compoents:该数组内包含的是RGB值构成的颜色数据，由于RGB值需要四个参数（red、green、blue、alpha），所以假如有三个颜色，则components应该有4*3=12个元素
+//    CGFloat compoents[12]={
+//        248.0/255.0,86.0/255.0,86.0/255.0,1,
+//        249.0/255.0,127.0/255.0,127.0/255.0,1,
+//        1.0,1.0,1.0,1.0
+//    };
+//    // locations:颜色所在位置（范围0~1），这个数组的个数不小于components中存放颜色的个数
+//    CGFloat locations[3]={0,0.3,1.0};
+//    // count:渐变个数，等于locations的个数
+//    // CGGradientCreateWithColorComponents 设置渐变各项参数 获得CGGradientRef对象
+//    CGGradientRef gradient= CGGradientCreateWithColorComponents(colorSpace, compoents, locations, 3);
+//    
+//    // startPoint:起始位置
+//    // endPoint:终止位置
+//    // options:绘制方式,kCGGradientDrawsBeforeStartLocation 开始位置之前就进行绘制，到结束位置之后不再绘制，kCGGradientDrawsAfterEndLocation开始位置之前不进行绘制，到结束点之后继续填充
+//    CGContextDrawLinearGradient(context, gradient, CGPointZero, CGPointMake(320, 300), kCGGradientDrawsAfterEndLocation);
+//    
+//    //释放颜色空间
+//    CGColorSpaceRelease(colorSpace);
+    
+//    UIImage *image=[UIImage imageNamed:@"1.png"];
     //从某一点开始绘制
 //    [image drawAtPoint:CGPointMake(10, 50)];
     //绘制到指定的矩形中，注意如果大小不合适会会进行拉伸
